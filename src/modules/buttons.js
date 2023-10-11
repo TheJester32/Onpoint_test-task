34a393e024bc5ef710ce59871fca8926a380e2f5
TheJester32 <bogdanpopoveducation@gmail.com>
2023-10-11 18:24:06 +0300

    button bug almost fixed
    button bug almost fixed

--- src/modules/buttons.js
+++ src/modules/buttons.js
@@ -1,6 +1,8 @@
-const sliderLine = document.querySelector('.slider__slider-line');
-
-export function secondSlide () {
-    const sliderWidth = 1024;
-    sliderLine.style.transform += `translate3d(${sliderWidth}, 0, 0)`;
-}
+export function secondSlide() {
+    const sliderLine = document.querySelector('.slider__slider-line');
+    const sliderSlide = document.querySelector('.slider__slide');
+    const sliderWidth = parseInt(sliderSlide.style.width);
+  
+    const newTransformValue = -sliderWidth;
+    sliderLine.style.transform = `translate3d(${newTransformValue}px, 0, 0)`;
+  }
