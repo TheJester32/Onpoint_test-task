7d2f8873ee56a0f1c5326c7ec5081d6868f9f72c
TheJester32 <bogdanpopoveducation@gmail.com>
2023-10-13 07:39:03 +0300

    project done (Prettiered)
    project done (Prettiered)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import './index.pug';
-import './index.scss';
-import bigSlider from './modules/bigSlider';
-import observeAnimatedPics from './modules/slide-2';
+import "./index.pug";
+import "./index.scss";
+import bigSlider from "./modules/bigSlider";
+import observeAnimatedPics from "./modules/slide-2";
+import closePopup from "./modules/closePopup";
+import openPopup from "./modules/openPopup";
+import changePopUpText from "./modules/popUpText";
 
-new bigSlider(document.getElementById('slider'));
+document.querySelector(".slider-3__pop-up").classList.add("hidden");
+
+new bigSlider(document.getElementById("slider"));
 
 observeAnimatedPics();
+closePopup();
+openPopup();
+changePopUpText();
